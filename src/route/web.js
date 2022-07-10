0f716e3e8726836165c5503f69cc27c308aa30e4
haih19 <haih19c@gmail.com>
2022-07-10 23:57:16 +0700

    update #8

---
+++ src/route/web.js
@@ -0,0 +1,18 @@
+import express from "express";
+import getHomePage from '../controller/homeController.js'
+//import function from file.js
+
+
+let router = express.Router()
+
+const initWebRoute = (app) => {
+    router.get('/', getHomePage)
+
+    router.get('/about', (req, res) => {
+        res.send('This is about route')
+    })
+
+    return app.use('/', router)
+}
+
+export default initWebRoute;
--- src/server.js
+++ src/server.js
@@ -1,24 +1,18 @@
 import express from 'express';
 import configViewEngine from './config/viewEngine.js';
 import { } from 'dotenv/config'
+import initWebRoute from './route/web.js';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+//create view engine
 configViewEngine(app);
+//init web route
+initWebRoute(app)
 
 
-app.get('/', (req, res) => {
-    res.render('index.ejs')
-})
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
-
-app.get('/about', (req, res) => {
-    res.send('This is about route')
-})
 
 
 app.listen(port, () => {
